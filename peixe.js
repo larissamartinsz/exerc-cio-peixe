f0e994ba55b4349eeb9f0abcf49cf6c7a95d5104
larissamartinsz <larissa.souza@sou.inteli.edu.br>
2024-02-09 08:50:55 -0300

    adição das tarefas

--- peixe.js
+++ peixe.js
@@ -1,7 +1,3 @@
-//respondendo a pergunta
-var resposta = "Eu já havia programado antes de forma básica utilizando css, html e JS, contudo, pela falta de prática tinha esquecido de muitas coisas. Sendo assim, essas primeiras atividades do módulo estão servindo para mim como uma revisão e aprofundamento, visto que apesar de ter visto algumas aulas sobre, tive poucas oportunidades de colocar o conhecimento em prática.";
-console.log(resposta);
-
 // estabelecendo configurações básicas
 var config = {
     type: Phaser.AUTO, //tipo de renderizador escolhido automaticamente
@@ -23,22 +19,26 @@ var peixinho; //criando a variável que armanezará o peixe
 
 function preload() {
     this.load.image('mar', 'assets/bg_azul-escuro.png'); //pre-carregando o fundo
-    this.load.image('logo', 'assets/logo-inteli_branco.png'); //pre-carregando a logo
+    this.load.image('logo', 'assets/logo-inteli_azul.png'); //pre-carregando a logo
     this.load.image('peixe', 'assets/peixes/tubarao.png'); //pre-carregando o peixe
+    this.load.image('surfista', 'assets/menina-surfista.png'); //pre-carregando elemento novo surfista
+    this.load.image('areia', 'assets/areia.png'); //pre-carregando areia
 }
 
 
 function create() {
     this.add.image(400, 300, 'mar'); //adicionando e posicionando o fundo
+    this.add.image(200, 200, 'surfista').setScale(0.7); //adicionando o novo elemento: surfista
+    this.add.image(400, 450, 'areia').setScale() //adicionando o novo elemento: areia
     this.add.image(400, 525, 'logo').setScale(0.5); //adicionando, ajustando a escala e posicionando a logo
     peixinho = this.add.image(400, 300, 'peixe'); //adicionando o peixe a variável e posicionando
     peixinho.setOrigin(0.5, 0.5).setFlip(true, false); //indicando o espelhamento horizontal da imagem
-
 }
 
 //adicionando controles no peixe
 function update() {
     peixinho.x = this.input.x;
     peixinho.y = this.input.y;
+
 }
 
